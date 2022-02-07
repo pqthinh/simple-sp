714cdff95a547e0d89465c8db0999436de6b840e
pqthinh <18021231@vnu.edu.vn>
2022-02-07 22:52:31 +0700

    get,post method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +16,14 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "test", defaultValue = "true") String test) {
+		if(test.equals("true")) return String.format("This just is test. Hello %s !", name);
 		return String.format("Hello %s!", name);
 	}
 
 	@GetMapping("/")
-	public String welcome() {
+	public String welcome(@RequestBody(required = false) String test) {
+		System.out.println("Test " + test.toString());
 		return "Hello";
 	}
 }
