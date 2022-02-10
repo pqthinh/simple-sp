3247948e923f1a84da2f0e6fa7e6d6663aa44e9b
pqthinh <18021231@vnu.edu.vn>
2022-02-11 01:35:36 +0700

    model | controller | repository

---
+++ src/main/java/com/example/demo/EmployeeApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeApplication.class, args);
+    }
+
+    @Bean
+    EmployeeRepository repository() {
+        EmployeeRepository repository = new EmployeeRepository();
+        repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
+        repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
+        repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
+        repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));
+        repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
+        repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));
+        repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
+        repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
+        repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
+        repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
+        return repository;
+    }
+
+}
